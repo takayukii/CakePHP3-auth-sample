b8fd8d34f8e338932bd6a5d60ff21840d1eccdd8
takayukii <takayukii@gmail.com>
2015-09-30 17:17:09 +0900

    implements Login

---
+++ src/Model/Table/CakeSessionsTable.php
@@ -0,0 +1,54 @@
+<?php
+namespace App\Model\Table;
+
+use App\Model\Entity\CakeSession;
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * CakeSessions Model
+ *
+ */
+class CakeSessionsTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->table('cake_sessions');
+        $this->displayField('id');
+        $this->primaryKey('id');
+
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->allowEmpty('id', 'create');
+
+        $validator
+            ->requirePresence('data', 'create')
+            ->notEmpty('data');
+
+        $validator
+            ->add('expires', 'valid', ['rule' => 'datetime'])
+            ->allowEmpty('expires');
+
+        return $validator;
+    }
+}
